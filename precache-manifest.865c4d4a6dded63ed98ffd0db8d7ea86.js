07c09affaba8a4ac0621f3439a988cad012461e1
parichkova <tyhomiraparichkova@gmail.com>
2019-05-31 00:09:18 +0300

    Updates

---
+++ precache-manifest.865c4d4a6dded63ed98ffd0db8d7ea86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfe4dd73f32251f620c1",
+    "url": "/weather-forecast/static/js/runtime~main.dfe4dd73.js"
+  },
+  {
+    "revision": "3b4b9b64f0972300e626",
+    "url": "/weather-forecast/static/js/main.3b4b9b64.chunk.js"
+  },
+  {
+    "revision": "ddee53e1d2b61a979291",
+    "url": "/weather-forecast/static/js/2.ddee53e1.chunk.js"
+  },
+  {
+    "revision": "3b4b9b64f0972300e626",
+    "url": "/weather-forecast/static/css/main.9f9b6cff.chunk.css"
+  },
+  {
+    "revision": "a44a9fa1e82094fedbe675e32ab236b7",
+    "url": "/weather-forecast/index.html"
+  }
+];
